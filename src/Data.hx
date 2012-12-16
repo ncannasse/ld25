4d1f36d2da2fb72aff8dcd34d09009f21bcc8066
ncannasse <ncannasse@motion-twin.com>
2012-12-16 23:32:24 +0100

    final version, missing music

--- src/Data.hx
+++ src/Data.hx
@@ -11,7 +11,7 @@ class Data
 	public static var WEAPONS : Array<ShopItem> = [
 		{ i : BaseBat, price : 10, f : function() game.hero.power = 2 },
 		{ i : Knife, price : 50, f : function() game.hero.power = 5 },
-		{ i : Pistol, price : 500, f : function() game.hero.power = 10 },
+		{ i : Pistol, price : 450, f : function() game.hero.power = 10 },
 		{ i : MiniGun, price : 5000, f : function() game.addAction(2) },
 	];
 	
@@ -19,7 +19,7 @@ class Data
 		{ id : 0, name : "JeeZee", age : 30, att : 5, def : 10, money : 5, quest : { targets : [7], t : "I'm in Love with Miss Auto, but she rejected me. Attack her and cut her beautiful hair for me !", m : 10 } },
 		{ id : 1, name : "Leela", age : 7, att : 0, def : 5, money : 3, quest : { targets : [2], t : "My brother is annoying, he won't stop complaining.\nIf you kill him I'll give you a reward.", m : 50, kill : true } },
 		{ id : 2, name : "Jimjim", age : 9, att : 1, def : 5, money : 3, quest : { targets : [7], t : "My mother is annoying, she won't stop yelling at me.\nIf you kill her I'll give you $100", m : 100, kill : true } },
-		{ id : 3, name : "Weido", age : 45, att : 2, def : 15, money : 10, quest : { targets : [8,4], t : "The two Mafia members Tizon and Glaze are threatening be.\nIf you could get rid of them I'll give you a huge reward", m : 4700, kill : true } },
+		{ id : 3, name : "Weido", age : 45, att : 2, def : 15, money : 10, quest : { targets : [8,4], t : "The two Mafia members Tizon and Glaze are threatening be.\nIf you could get rid of them I'll give you a huge reward", m : 4800, kill : true } },
 		{ id : 4, name : "Tizon", age : 35, att : 50, def : 40, money : 5, quest : { targets : [5], t : "You are doing a good job, but Glaze is betraying me : get a knife and kill him !", m : 50, kill : true } },
 		{ id : 5, name : "Glaze", age : 30, att : 10, def : 15, money : 20, quest : { targets : [4], t : "Our Boss wanna see ya, move your ass and see him quick", m : 0 } },
 		{ id : 6, name : "Bob", age : 22, att : 5, def : 20, money : 20, quest : { targets : [6], t : "I hate these truks, they won't let me skate freely.\nDestroying them would require a MiniGun...", m : 50 } },
